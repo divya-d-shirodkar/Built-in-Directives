42c13801ed5bc0779f29145892e1f508b8c01683
Divya Shirodkar <divya.shirodkar@klm.com>
2017-11-30 11:36:36 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgForComponent,
+    NgIfComponent,
+    NgSwitchComponent,
+    NgStyleComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule
